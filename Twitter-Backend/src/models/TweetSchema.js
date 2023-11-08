77742acc2ca094f76296a49a541cc5527619612b
PedroAre88 <106494143+PedroAre88@users.noreply.github.com>
2023-11-08 01:36:13 -0400

    Delete Twitter-Backend/src/models/TweetSchema.js

--- Twitter-Backend/src/models/TweetSchema.js
+++
@@ -1,7 +0,0 @@
-const TweetSchema = new mongoose.Schema({
-    content: String,
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-    created_at: Date,
-    likes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
-  });
-  
