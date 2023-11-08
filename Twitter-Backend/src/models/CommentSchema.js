da268f8ced01447ed37b1ff6f0700c9db0133349
PedroAre88 <106494143+PedroAre88@users.noreply.github.com>
2023-11-08 01:34:49 -0400

    Delete Twitter-Backend/src/models/CommentSchema.js

--- Twitter-Backend/src/models/CommentSchema.js
+++
@@ -1,7 +0,0 @@
-const CommentSchema = new mongoose.Schema({
-    content: String,
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-    tweet: { type: mongoose.Schema.Types.ObjectId, ref: 'Tweet' },
-    created_at: Date
-  });
-  
